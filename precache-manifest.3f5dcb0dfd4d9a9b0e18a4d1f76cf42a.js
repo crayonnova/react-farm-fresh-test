869905d8b8e9687bbd13591729ff86ca36f8255a
emo lay <julyemolay77@gmail.com>
2020-02-24 15:00:49 +0630

    Updates

---
+++ precache-manifest.3f5dcb0dfd4d9a9b0e18a4d1f76cf42a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc8e57d3156c79211cbecc9f8bd9011",
+    "url": "/react-farm-fresh-test/index.html"
+  },
+  {
+    "revision": "37f45ad998395d339584",
+    "url": "/react-farm-fresh-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7bb8ad66803b57fab2e4",
+    "url": "/react-farm-fresh-test/static/js/2.150b3053.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-farm-fresh-test/static/js/2.150b3053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f45ad998395d339584",
+    "url": "/react-farm-fresh-test/static/js/main.5b7bf30d.chunk.js"
+  },
+  {
+    "revision": "5b58f26e7be54f57ac47",
+    "url": "/react-farm-fresh-test/static/js/runtime-main.9e30bd81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-farm-fresh-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
